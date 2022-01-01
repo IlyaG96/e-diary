a0de1ea679f8da22016ee8af7ac1003f9dafbc60
IlyaG96 <ilyag_96@icloud.com>
2022-01-01 18:32:53 +1100

    add func to correct chastisements

--- correct_points.py
+++ correct_points.py
@@ -1,14 +1,24 @@
 from datacenter.models import Schoolkid
 from datacenter.models import Mark
+from datacenter.models import Chastisement
+
+schoolkid = Schoolkid()
+name = "Голубев Феофан Владленович"
 
 
-def correct_points():
-    pupil = Schoolkid.objects.filter(full_name__contains="Фролов Иван")
+def correct_points(schoolkid, name):
+    pupil = schoolkid.objects.filter(full_name__contains=name)
     pupil = pupil[0]
     marks_query = Mark.objects.filter(schoolkid=pupil, points__lte=3)
     marks_total = marks_query.count()
     for index in range(marks_total):
         mark_to_correct = marks_query[index]
-        mark_to_correct.points = 4
+        mark_to_correct.points = 5
         mark_to_correct.save()
 
+
+def remove_chastisements(schoolkid, name):
+    pupil = Schoolkid.objects.filter(full_name__contains=name)
+    pupil = pupil[0]
+    chastisements = Chastisement.objects.filter(schoolkid=pupil)
+    chastisements.delete()
