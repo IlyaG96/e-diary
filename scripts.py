46fe6cbaebc5480476e5213c80ca06e36a348d59
IlyaG96 <ilyag_96@icloud.com>
2022-01-02 17:45:21 +1100

    add cli interface

--- scripts.py
+++ scripts.py
@@ -1,5 +1,6 @@
 import random
-
+import sys
+from pprint import pprint
 from datacenter.models import Schoolkid
 from datacenter.models import Mark
 from datacenter.models import Chastisement
@@ -14,7 +15,7 @@ commendations = [
     "Именно этого я давно ждал от тебя!", "Сказано здорово – просто и ясно!",
     "Ты, как всегда, точен!", "Очень хороший ответ!", "Талантливо!",
     "Ты сегодня прыгнул выше головы!", "Я поражен!", "Уже существенно лучше!",
-    "Потрясающе!", "Замечательно!",  "Прекрасное начало!", "Так держать!",
+    "Потрясающе!", "Замечательно!", "Прекрасное начало!", "Так держать!",
     "Ты на верном пути!", "Здорово!", "Это как раз то, что нужно!",
     "Я тобой горжусь!", "С каждым разом у тебя получается всё лучше!",
     "Мы с тобой не зря поработали!", "Я вижу, как ты стараешься!",
@@ -32,14 +33,14 @@ subjects = [
 
 def chose_pupil():
     pupil = None
+    print("Вас приветствует система взлома электронного дневника!\n"
+          "Введите, пожалуйста, свои фамилию и имя через пробел")
     while not pupil:
-        name = input("Вас приветствует система взлома электронного дневника!\n"
-                     "Введите, пожалуйста, свои фамилию и имя через пробел\n"
-                     ":")
-        pupil = Schoolkid.objects.filter(full_name__contains=name)
+        name = input(":")
+        pupil = Schoolkid.objects.filter(full_name__contains=name.title())
         if not pupil:
             print("Такой ученик в школе не учится. Попробуйте еще раз или уточните запрос\n"
-                  " (возможно, стоит поменять фамилию и имя местами)")
+                  "(возможно, стоит поменять фамилию и имя местами)")
         elif len(pupil) > 1:
             print("Найдено несколько учеников, уточните запрос!")
             pupil = None
@@ -50,7 +51,7 @@ def chose_pupil():
 
 def correct_points(pupil):
     marks = Mark.objects.filter(schoolkid=pupil, points__lte=3)
-    for num, mark in marks.count():
+    for num in range(marks.count()):
         mark_to_correct = marks[num]
         mark_to_correct.points = 5
         mark_to_correct.save()
@@ -65,27 +66,42 @@ def create_commendation(commendation_subject, pupil):
 
     subject = Subject.objects.filter(year_of_study=pupil.year_of_study,
                                      title=commendation_subject).first()
+
     pupils_subject = Lesson.objects.filter(year_of_study=pupil.year_of_study,
-                                           group_letter=pupil.group_lette,
-                                           subject=subject).first()
-    teacher_id = Teacher.objects.filter(full_name=pupils_subject.teacher).first()
+                                           group_letter=pupil.group_letter,
+                                           subject=subject)
+
+    random_date_subject = random.choice(pupils_subject)
+    teacher_id = Teacher.objects.filter(full_name=random_date_subject.teacher).first()
     commendation = Commendation.objects.filter(subject=subject,
                                                schoolkid=pupil,
-                                               created=pupils_subject.date)
+                                               created=random_date_subject.date)
+
     commendation.create(text=random.choice(commendations),
-                        created=pupils_subject.date,
+                        created=random_date_subject.date,
                         schoolkid=pupil,
                         teacher=teacher_id,
                         subject=subject)
 
 
 while True:
+    choice, commendation_subject = "", ""
     pupil = chose_pupil()
-    action = int(input("Введите желаемое действие: \n"
-                       "1 - Исправить оценки\n"
-                       "2 - Удалить замечания\n"
-                       "3 - Создать похвалу\n"
-                       ":"))
-    if action not in range(1, 4):
-        commendation_subject = input("Напишите название предмета, на котором вас надо похвалить")
+    correct_points(pupil)
+    remove_chastisements(pupil)
+    print("Оценки исправлены, замечания удалены")
+    while choice != "нет":
+        if choice not in ("да", "нет"):
+            choice = input("Как насчет того, чтобы похвалить себя любимого?\n"
+                           "Да/Нет?: ").lower()
+        if choice == "нет":
+            sys.exit("Завершаю работу. Не забудьте удалить следы своего присутствия на сайте!")
+        while not commendation_subject:
+            pprint(subjects)
+            commendation_subject = input("Выберите название предмета из списка: ")
+            if commendation_subject in subjects:
+                create_commendation(commendation_subject, pupil)
+            else:
+                print("Где-то опечатка. Сверьтесь с названием из списка, а лучше скопируйте его")
+            commendation_subject = None
 
